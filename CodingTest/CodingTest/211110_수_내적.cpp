f50a8a215963f69741b68d15cbb3a33110cf6723
JeonGeunHee <weiding11086@gmail.com>
2021-11-10 17:55:50 +0900

    update

---
+++ CodingTest/CodingTest/211110_수_내적.cpp
@@ -0,0 +1,30 @@
+﻿//Start Time	: 17 50
+//End Time		: 17 51 
+//통과 유뮤		: 17 51
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int solution(vector<int> a, vector<int> b)
+{
+	int answer = 0;
+
+	for (int i = 0; i < (int)a.size(); ++i)
+	{
+		answer += a[i] * b[i];
+	}
+
+	return answer;
+}
+
+void main()
+{
+
+
+	system("pause");
+}
